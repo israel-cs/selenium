c7d4ca950a4d89723d84dac15059b8b237b6bad1
Israel <israel.info@gmail.com>
2022-09-08 20:26:21 -0300

    ok

--- HelloWorldSelenium.java
+++ HelloWorldSelenium.java
@@ -11,9 +11,7 @@ public class HelloWorldSelenium {
 		WebDriver browser = new ChromeDriver();
 		browser.navigate().to("http://localhost:8080/leiloes");
 
-		System.out.println("oká");
-
-		
+		System.out.println("oká");		
 
 		browser.quit();
 	}
